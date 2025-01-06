77bf6e886eb87b5e993d32184d55b81034229c57
sol <nathf1005@naver.com>
2025-01-06 17:22:40 +0900

    연산자 숫자 생성

--- app/index.tsx
+++ app/index.tsx
@@ -1,15 +1,19 @@
-import { Text, View } from "react-native";
+import Calculator from "@/src/Calculator";
+import { SafeAreaView, StyleSheet } from "react-native";
 
 export default function Index() {
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
-    </View>
+    <SafeAreaView style={styles.container}>
+      <Calculator />
+    </SafeAreaView>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+});
